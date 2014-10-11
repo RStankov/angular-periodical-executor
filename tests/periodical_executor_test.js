872553d37657ab299000201f24bc1be8340e0b66
Radoslav Stankov <rstankov@gmail.com>
2014-10-11 13:49:57 +0300

    Initial implemenation of PeriodicalExecutor

--- tests/periodical_executor_test.js
+++ tests/periodical_executor_test.js
@@ -11,4 +11,32 @@ function loadModule(module, name) {
 }
 
 describe('PeriodicalExecutor', function() {
+  this.timeout(10);
+
+  it("executes callback periodically", function(done) {
+    var times = 0;
+    new PeriodicalExecutor(1, function() {
+      times += 1;
+      if (times == 2) {
+        done();
+      }
+    });
+  });
+
+  it("can be stopped from .stop", function(done) {
+    var timer = new PeriodicalExecutor(2, function() {
+      throw "this should not be executed";
+    });
+
+    timer.stop();
+
+    setTimeout(done, 5);
+  });
+
+  it("can be restarted with .start", function(done) {
+    var timer = new PeriodicalExecutor(2, done);
+
+    timer.stop();
+    timer.start();
+  });
 });
