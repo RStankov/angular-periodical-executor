11d05546bbc11c84f38ddeb76ac73a97be2612e4
Radoslav Stankov <rstankov@gmail.com>
2014-10-17 11:24:09 +0300

    Tests clean up

--- tests/periodical_executor_test.js
+++ tests/periodical_executor_test.js
@@ -16,31 +16,37 @@ describe('PeriodicalExecutor', function() {
 
   var PeriodicalExecutor = loadModule('PeriodicalExecutor');
 
-  it("executes callback periodically", function(done) {
-    var times = 0;
-    new PeriodicalExecutor(1, function() {
-      times += 1;
-      if (times == 2) {
-        done();
-      }
+  describe("#constructor", function() {
+    it("executes callback periodically", function(done) {
+      var times = 0;
+      new PeriodicalExecutor(1, function() {
+        times += 1;
+        if (times == 2) {
+          done();
+        }
+      });
     });
   });
 
-  it("can be stopped from .stop", function(done) {
-    var timer = new PeriodicalExecutor(2, function() {
-      throw "this should not be executed";
+  describe("#start", function() {
+    it("start the timer", function(done) {
+      var timer = new PeriodicalExecutor(2, done);
+
+      timer.stop();
+      timer.start();
     });
-
-    timer.stop();
-
-    setTimeout(done, 5);
   });
 
-  it("can be restarted with .start", function(done) {
-    var timer = new PeriodicalExecutor(2, done);
+  describe("#stop", function() {
+    it("stops the timer", function(done) {
+      var timer = new PeriodicalExecutor(2, function() {
+        throw "this should not be executed";
+      });
 
-    timer.stop();
-    timer.start();
+      timer.stop();
+
+      setTimeout(done, 5);
+    });
   });
 
   describe("#isRunning", function() {
