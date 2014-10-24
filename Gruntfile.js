d22321013586afca6cb1ca0fedb6db5f9e80d0a5
Radoslav Stankov <rstankov@gmail.com>
2014-10-24 16:00:56 +0300

    Add karma and grunt for testing

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    karma: {
+      options: {
+        configFile: 'tests/karma.conf.js'
+      },
+      build: {
+        singleRun: true,
+        autoWatch: false
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('test', ['karma:build']);
+};
+
